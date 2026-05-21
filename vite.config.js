8cdde9000a209a1e9bc0940133445f4719cdf3e7
kraftkartel <kraftkartel20@gmail.com>
2026-05-21 06:20:38 -0700

    Remove fsevents from Rollup build options
    Removed fsevents handling from Rollup options.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['fsevents'],
-      onwarn(warning, warn) {
-        if (warning.ids && warning.ids.some(id => id.includes('fsevents'))) return;
-        if (warning.message.includes('fsevents')) return;
-        warn(warning);
-      }
-    }
-  }
 });
