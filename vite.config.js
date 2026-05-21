9d38b128ccde8a577fffe54b50d0708a19af16e9
kraftkartel <kraftkartel20@gmail.com>
2026-05-21 05:58:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // This tells Rollup to ignore the fsevents module
+      external: ['fsevents']
+    }
+  }
+});
