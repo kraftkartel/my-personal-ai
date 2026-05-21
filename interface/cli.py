e408861ace0ddebd8de796f1c7918b5bb9120db9
kraftkartel <kraftkartel20@gmail.com>
2026-05-21 08:42:45 +0200

    commit

--- interface/cli.py
+++ interface/cli.py
@@ -3,7 +3,10 @@ sys.path.insert(0, '/content/project')
 
 from modules.chat_model import generate_response
 from modules.chat_rules import apply_rules
-
+from modules.image_reader import read_image
+from modules.image_editor import resize_image, convert_to_gray
+from modules.code_model import generate_code
+from modules.sandbox import run_code_safely
 def main():
     print("🤖 Personal AI CLI")
     while True:
